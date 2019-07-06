e51677f69f956b792de3cedfbd71cbaf20307a2a
Mahesh Vira <vira.mahesh@gmail.com>
2019-07-06 11:28:37 -0400

    appointment-api config property initial commit

--- src/main/java/com/vira/mahesh/api/config/server/VisitorManagementConfigServerApplication.java
+++ src/main/java/com/vira/mahesh/api/config/server/VisitorManagementConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.vira.mahesh.api.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class VisitorManagementConfigServerApplication {
 
 	public static void main(String[] args) {
