f27454a5f93c265fac46e79aceeb0411d033c8ee
Mahesh Vira <vira.mahesh@gmail.com>
2019-07-06 09:09:51 -0400

    Initial Commit

---
+++ src/main/java/com/vira/mahesh/api/config/server/VisitorManagementConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.vira.mahesh.api.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorManagementConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorManagementConfigServerApplication.class, args);
+	}
+
+}
